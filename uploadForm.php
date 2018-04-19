af1864f28ffeafe8e5aca8ed4419c9a4950e3064
MinhPhu0304 <Lminhphu0304@gmail.com>
2018-04-19 16:38:16 +1200

    Add files via upload

---
+++ uploadForm.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<form action="uploadService.php" method="post" enctype="multipart/form-data">
+    Select file to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload file" name="Submit">
+</form>
+
+</body>
+</html> 
---
+++ welcome.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<style>
+
+body {
+    background-color: DodgerBlue;
+}
+
+p {
+	text-align:center;
+    font-family: verdana;
+    font-size: 30px;
+	color: red;
+}
+</style>
+<meta charset="UTF-8">
+<title>Welcome to Minh place</title>
+</head>
+<body>
+<p>This is Login page</p>
+<a href = "/form/index">Data <br>
+<a href = "uploadForm.php">Upload File <br>
+<a href = "index.php">Log out
+</body>
+</html>
